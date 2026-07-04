0cc3dda3093a90b7fa9cadbaddcb5bd0c4eb9259
dongyp30 <dongyp30@chinaunicom.cn>
2026-07-04 17:57:25 +0800

    feat: 赛事二维码长按保存/分享 + 文档更新

--- cloudfunctions/genRaceQR/index.js
+++ cloudfunctions/genRaceQR/index.js
@@ -8,6 +8,7 @@ exports.main = async (event) => {
     page: 'pages/tools/calendar/detail',
     width: 280,
     checkPath: false,
+    env_version: 'trial',
   });
   const upload = await cloud.uploadFile({
     cloudPath: `qrcode/race_${raceId}.png`,
